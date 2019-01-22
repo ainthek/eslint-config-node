c55be376fbee801906ad9995d651eb315303744d
Martin Marko <marcus@gratex.com>
2019-01-22 12:46:21 +0100

    you can now split config with eslint-split-rules, related to #1

---
+++ tools/eslint-split-rules.js
@@ -0,0 +1,24 @@
+/* 
+gets all loaded rules,
+and creates split config by "category"	
+*/
+
+const Rules = require("eslint/lib/rules");
+const loadedRules = new Rules().getAllLoadedRules();
+
+const slugify = require("slugify");
+const fs = require("fs");
+
+// loaded rules is map, this is map->{c1:{},c2:{},...}
+let r = {};
+for (const [key, value] of loadedRules) {
+  let category = value.meta.docs.category;
+  let type = value.meta.type;
+  r[category] || (r[category] = {});
+  r[category][type] || (r[category][type] = {})
+  r[category][type][key] = "off";
+}
+let out = `${__dirname}/eslintrc.split.js`;
+fs.writeFile(out, JSON.stringify(r, 0, 2), function() {
+  console.log("done", out);
+})
