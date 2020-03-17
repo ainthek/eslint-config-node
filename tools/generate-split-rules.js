79a84ac5f2718ae4ca6cb7646e55603d16816597
Martin Marko <marcus@gratex.com>
2020-03-17 19:30:06 +0100

    adjusted for changed api of eslint 6.0

--- tools/generate-split-rules.js
+++ tools/generate-split-rules.js
@@ -10,22 +10,35 @@ const { CLIEngine } = require("eslint");
 const cli = new CLIEngine();
 
 function getAllLoadedRules() {
-  const cli = new CLIEngine();
-  const rules1 = [...cli.getRules()]; //unusable
-  const cfg = cli.getConfigForFile("FOO.js");
-  //console.log(cfg);
+  // const cli = new CLIEngine();
+  // const rules1 = [...cli.getRules()]; //unusable
+  // const cfg = cli.getConfigForFile("FOO.js");
+  // //console.log(cfg);
 
-  const { globals, extends: _extends, /*others*/ ...others } = cfg;
+  // const { globals, extends: _extends, /*others*/ ...others } = cfg;
 
+  // const cli2 = new CLIEngine({
+  //   baseConfig: {
+  //     globals,
+  //     extends: _extends
+  //   },
+  //   ...others
+  // });
   const cli2 = new CLIEngine({
     baseConfig: {
-      globals,
-      extends: _extends
-    },
-    ...others
+      extends: [
+        "eslint:all",
+        "plugin:node/recommended"
+      ]
+    }
   });
+
   return cli2.getRules(); //usable
 }
+
+const slugify = require("slugify");
+const fs = require("fs");
+
 // loaded rules is map, this is map->{c1:{},c2:{},...}
 let r = {};
 for (const [key, value] of getAllLoadedRules()) {
@@ -39,4 +52,7 @@ for (const [key, value] of getAllLoadedRules()) {
     r["deprecated"][key] = "off";
   }
 }
-console.log(JSON.stringify(r, 0, 2))
+let out = `./eslintrc.split.original.js`;
+fs.writeFile(out, JSON.stringify(r, 0, 2), function() {
+  console.log("done", out);
+})
