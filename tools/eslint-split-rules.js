ed187b613473ecf452c405597c6e12fc81ab54a6
Martin Marko <marcus@gratex.com>
2020-03-17 19:50:26 +0100

    Fixed split tool, now reports project values

--- tools/eslint-split-rules.js
+++ tools/eslint-split-rules.js
@@ -7,36 +7,44 @@ and creates split config by "category"
 */
 
 const { CLIEngine } = require("eslint");
+
+//function getAllLoadedRules() {
 const cli = new CLIEngine();
+const rules1 = cli.getRules(); //unusable
 
-function getAllLoadedRules() {
-  const cli = new CLIEngine();
-  const rules1 = [...cli.getRules()]; //unusable
-  const cfg = cli.getConfigForFile("FOO.js");
-  //console.log(cfg);
+const cfg = cli.getConfigForFile("FOO.js");
 
-  const { globals, extends: _extends, /*others*/ ...others } = cfg;
 
-  const cli2 = new CLIEngine({
-    baseConfig: {
-      globals,
-      extends: _extends
-    },
-    ...others
-  });
-  return cli2.getRules(); //usable
-}
+const { globals, extends: _extends, /*others*/ ...others } = cfg;
+
+const cli2 = new CLIEngine({
+  baseConfig: {
+    globals,
+    extends: _extends
+  },
+  ...others
+});
+//return cli2.getRules(); //usable
+//}
 // loaded rules is map, this is map->{c1:{},c2:{},...}
 let r = {};
-for (const [key, value] of getAllLoadedRules()) {
+for (const [key, value] of cli2.getRules()) {
   let category = value.meta.docs.category;
   let type = value.meta.type;
   r[category] || (r[category] = {});
   r[category][type] || (r[category][type] = {})
-  r[category][type][key] = "error";
+  //r[category][type][key] = "error";
+  r[category][type][key] = simplify(cfg.rules[key]);
+
   if (value.meta.deprecated) {
     r["deprecated"] || (r["deprecated"] = {});
-    r["deprecated"][key] = "off";
+    //r["deprecated"][key] = "off";
+    r["deprecated"][key] = simplify(cfg.rules[key]);
   }
 }
-console.log(JSON.stringify(r, 0, 2))
+console.log(JSON.stringify(r, null, 2));
+
+
+function simplify(v) {
+  return Array.isArray(v) && v.length === 1 ? v[0] : v;
+}
